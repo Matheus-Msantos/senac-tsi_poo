ed79e33267f6368442afd636aa1c00480141c3d0
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-03-23 19:24:30 -0300

    Abstract, construct ...

--- ex1.php
+++ ex1.php
@@ -28,8 +28,27 @@ class titulo extends Nba {
         return $this->trofeu = $Qtd;
     }
 }
-$clippers = new titulo;
+
+class maiorTitulo {
+    private $objTitulo;
+
+    public function __construct($objTitulo) {
+        $this->objTitulo = $objTitulo;
+    }
+
+    public function __destruct() {
+        echo'Destrutor executado';
+    }
+}
+
+$titulo = new titulo;
 
 $titulo->setNome('Clippers');
 
+$celtics = new maiorTitulo($titulo);
+
 echo 'O ' . $titulo->showNome() . ' não tem troféu';
+
+unset($celtics);
+
+echo'Programa ainda rodando';
