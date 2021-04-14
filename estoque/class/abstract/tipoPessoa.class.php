2ae9eebda3ae67964e9dc69235b6edfe92a97446
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-04-13 22:17:49 -0300

    Criando Insert e Update para usuarios

--- estoque/class/abstract/tipoPessoa.class.php
+++ estoque/class/abstract/tipoPessoa.class.php
@@ -1,6 +1,13 @@
 <?php
 
-abstract class TipoPessoa {
+require_once'dataBase.class.php';
+
+abstract class TipoPessoa extends DataBase {
     protected $id;
     protected $nome;
+
+    public function __construct() {
+
+        parent::__construct();
+    }
 }
--- estoque/index.php
+++ estoque/index.php
@@ -8,14 +8,64 @@ require('class/movimentacao.class.php');
 class Main {
 
     public function __construct() {
+        echo "\n --- Iniciando programa --- \n";
         $objUsuario = new Usuario;
         $objFabricante = new Fabricante;
         $objEstoque = new Estoque;
         $objMovimentacao = new Movimentacao;
+
+        switch($_SERVER['argv'][1]) { //pega o segundo argumento passado pelo usuario via linha de comando
+            case'saveUsuario':
+                
+                $this->saveUsuario($objUsuario);
+                break;
+            case'editUsuario':
+                
+                $this->editUsuario($objUsuario);
+                break;
+            default:
+                echo "\n Não existe a funcionalidade {$_SERVER['argv'][1]} \n";
+        }
     }
 
-    public function __desconstruct () {
-        echo "rodou liso";
+    public function trataDados() {
+        $args = explode(',', $_SERVER['argv'][2]); //dados passado pelo usuario
+
+        foreach ($args as $valor) {
+            $arg = explode('=', $valor);
+
+            $dados[$arg[0]] = $arg[1];
+
+        }
+
+        return $dados;
+    }
+
+    public function saveUsuario($objUsuario) {
+
+        $dados = $this->trataDados();
+
+        $objUsuario->setDados($dados);
+        if($objUsuario->saveDados()) {
+            sleep(1);
+            echo "\n --- Usuário gravado --- \n";
+        }
+    }
+
+    public function editUsuario($objUsuario) {
+
+        $dados = $this->trataDados();
+
+        $objUsuario->setDados($dados);
+        if($objUsuario->saveDados()) {
+            sleep(1);
+            echo "\n --- Usuário editado --- \n";
+        }
+    }
+
+    public function __destruct () {
+        sleep(1);
+        echo "\n --- Fim do programa --- \n";
     }
 }
 
